bbf6bef724e9acff3ec55859cab895b09f50f920
Enne <102458482+Enne-chan@users.noreply.github.com>
2022-04-24 09:13:15 +0700

    Delete session.js

--- session.js
+++
@@ -1,6 +0,0 @@
-{
-   "clientID": "uziEMpwfNi9IuItK2SOzrA==",
-   "serverToken": "1@opquzRy1HbZua8C2TeXMrksD8cvnO75FvXNRXl8XFMc9aOT0D652
-   "clientToken": "5CGF14R5uc6Huj/ReYvg+eAx5tpdBgqZJ8rltDrvJrc=",
-   "encKey": 
-   
