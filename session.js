9906f9ebe3a6d3ad129172af064f2c6e764307e1
Enne <102458482+Enne-chan@users.noreply.github.com>
2022-04-24 09:13:06 +0700

    Update session.js

--- session.js
+++ session.js
@@ -2,6 +2,5 @@
    "clientID": "uziEMpwfNi9IuItK2SOzrA==",
    "serverToken": "1@opquzRy1HbZua8C2TeXMrksD8cvnO75FvXNRXl8XFMc9aOT0D652
    "clientToken": "5CGF14R5uc6Huj/ReYvg+eAx5tpdBgqZJ8rltDrvJrc=",
-   "encKey": "+xANR/Q0uZJi8aQHrVFkBrHcxt+Y86QqdpyZwwdiVZw=",
-   "macKey": "oODTxNa/q3vOqkbdYSAjVRC01BxvW1JtoM/vzJ5a9sk=
-}
+   "encKey": 
+   
